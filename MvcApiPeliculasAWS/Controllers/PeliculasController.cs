357e38f5ef9fd1a33cd00f2955a16645f902bae4
hmar321 <hmauricio.ar@gmail.com>
2024-05-28 12:40:01 +0200

    Add project files.

---
+++ MvcApiPeliculasAWS/Controllers/PeliculasController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiPeliculasAWS.Models;
+using MvcApiPeliculasAWS.Service;
+
+namespace MvcApiPeliculasAWS.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        private ServiceApiPeliculas service;
+
+        public PeliculasController(ServiceApiPeliculas service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Pelicula> pelis = await this.service.GetPeliculasAsync();
+            return View(pelis);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(string actor)
+        {
+            List<Pelicula> pelis = await this.service.FindPeliculasActorAsync(actor);
+            return View(pelis);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            Pelicula peli = await this.service.FindPeliculaAsync(id);
+            return View(peli);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Pelicula pelicula)
+        {
+            await this.service.CreatePeliculaAsync(pelicula);
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            Pelicula pelicula = await this.service.FindPeliculaAsync(id);
+            return View(pelicula);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Pelicula pelicula)
+        {
+            await this.service.UpdatePeliculaAsync(pelicula);
+            return RedirectToAction("Details", new { id = pelicula.IdPelicula });
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            await this.service.DeletePeliculaAsync(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcApiPeliculasAWS/Service/ServiceApiPeliculas.cs
@@ -0,0 +1,92 @@
+﻿using MvcApiPeliculasAWS.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace MvcApiPeliculasAWS.Service
+{
+    public class ServiceApiPeliculas
+    {
+        private string UrlApi;
+        private MediaTypeWithQualityHeaderValue Header;
+
+        public ServiceApiPeliculas(IConfiguration config)
+        {
+            this.UrlApi = config.GetValue<string>("ApiUrls:ApiPeliculasAWS");
+            this.Header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        private async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                HttpResponseMessage response = await client.GetAsync(this.UrlApi + request);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+
+        public async Task<List<Pelicula>> GetPeliculasAsync()
+        {
+            string request = "api/peliculas";
+            return await this.CallApiAsync<List<Pelicula>>(request);
+        }
+
+        public async Task<Pelicula> FindPeliculaAsync(int id)
+        {
+            string request = "api/peliculas/" + id;
+            return await this.CallApiAsync<Pelicula>(request);
+        }
+
+        public async Task<List<Pelicula>> FindPeliculasActorAsync(string actor)
+        {
+            string request = "api/peliculas/PeliculasActor/" + actor;
+            return await this.CallApiAsync<List<Pelicula>>(request);
+        }
+
+        public async Task CreatePeliculaAsync(Pelicula pelicula)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/peliculas";
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                string json = JsonConvert.SerializeObject(pelicula);
+                StringContent content = new StringContent(json, this.Header);
+                HttpResponseMessage response = await client.PostAsync(this.UrlApi + request, content);
+            }
+        }
+
+        public async Task UpdatePeliculaAsync(Pelicula pelicula)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/peliculas";
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                string json = JsonConvert.SerializeObject(pelicula);
+                StringContent content = new StringContent(json, this.Header);
+                HttpResponseMessage response = await client.PutAsync(this.UrlApi + request, content);
+            }
+        }
+
+        public async Task DeletePeliculaAsync(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/peliculas/" + id;
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                HttpResponseMessage response = await client.DeleteAsync(this.UrlApi + request);
+            }
+        }
+    }
+}
